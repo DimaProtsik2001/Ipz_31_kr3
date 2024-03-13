88f46b502c11cd4b5a00b602c03be44f77399c98
Dimasik2_0_0_1 <dimaprocik2001@gmail.com>
2024-03-13 12:36:37 +0200

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ipz_31_kr3"
+include(":app")
+ 
