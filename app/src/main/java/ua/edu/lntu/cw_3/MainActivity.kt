2e506b6b81f15525888a9803253cabf1acbb0f95
Dimasik2_0_0_1 <dimaprocik2001@gmail.com>
2024-03-13 15:05:26 +0200

    Додав всі потрібні Import

--- app/src/main/java/ua/edu/lntu/cw_3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_3/MainActivity.kt
@@ -5,27 +5,34 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import ua.edu.lntu.cw_3.ui.theme.Ipz_31_kr3Theme
 
 class MainActivity : ComponentActivity() {
@@ -56,7 +63,6 @@ fun MyApp()
             Item(index = index)
         }
     }
-
 }
 
 @Composable
@@ -65,26 +71,29 @@ fun Item(index: Int)
     var isActive by remember { mutableStateOf(false) }
     val cardHeight = if (isActive) 225.dp else 100.dp
 
-
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .height(cardHeight)
-            .clickable { isActive = !isActive },
+            .clickable { isActive = !isActive   },
     ) {
         Column(
             modifier = Modifier.padding(16.dp)
         ) {
-            Text()
+            Text("Назва картки$index", style = MaterialTheme.typography.bodyLarge)
+            Spacer(modifier = Modifier.height(8.dp))
             Image(
-
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(120.dp)
             )
+            Spacer(modifier = Modifier.height(8.dp))
             if (isActive)
             {
-                Text()
+                Text("Опис для картки $index", modifier = Modifier.fillMaxHeight())
             }
-
         }
     }
-
 }
